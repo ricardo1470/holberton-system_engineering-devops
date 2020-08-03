987273b334b8cfbf43a12715334c04adc1c36e93
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-03 16:12:09 -0500

    update file
    install puppet-lint manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install package pupped
 # exec resource executes an apt-get update command.
-# package resource installs the package puppedt and puppet-lint, 
+# package resource installs the package puppet-lint, 
 # defining that the apt-get update resource is a requirement,
 exec { 'apt-get update':
   command   => '/usr/bin/apt-get update'
