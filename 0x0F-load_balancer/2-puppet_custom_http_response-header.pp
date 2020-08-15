5166bc4d4b43f18ff05c6f655841f85e51ac0b0e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-15 01:58:17 +0000

    update file

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -5,22 +5,22 @@
 # -Write 2-puppet_custom_http_response-header.pp so that
 # it configures a brand new Ubuntu machine to the requirements asked in this task
 
-exec { 'update':
+exec {'update':
   command  => 'sudo apt-get update',
   provider => shell,
 }
 
-package { 'nginx':
+package {'nginx':
   ensure  => installed,
-  require => Exec['update']
+  require => Exec['update'],
 }
 
-file_line { 'header':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'listen 80 default_server;',
-  line     => 'add_header X-Served-By "$HOSTNAME";',
-  require  => Package['nginx'],
+file_line {'response_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
 }
 
 service { 'nginx':
